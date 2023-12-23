34336295ac8511e4487f67b823e933fc63821aa6
marvelousdre <110522817+marvelousdre@users.noreply.github.com>
2023-12-22 22:01:44 -0600

    Added show more button on smaller screens

--- index.js
+++ index.js
@@ -4,3 +4,15 @@ function toggleMenu() {
     profileMenu.classList.toggle("open-menu");
 }
 
+let sideActivity = document.getElementById("sidebarActivity");
+let moreLink = document.getElementById("showMoreLink");
+
+function toggleActivity() {
+    sideActivity.classList.toggle("open-activity");
+
+    if(sideActivity.classList.contains("open-activity")) {
+        moreLink.innerHTML = "Show less <b>-</b>";    
+    } else {
+        moreLink.innerHTML = "Show more <b>+</b>";
+    }
+};
